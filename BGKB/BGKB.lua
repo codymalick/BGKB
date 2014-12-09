17d537cd5dffe6d6656afc837bce324655a40453
Cody Malick <cody.malick@gmail.com>
2014-12-08 17:38:09 -0800

    Added BG Detection
    Added BG detection to activate and deactivate sounds and counters.
    Counters now reset on exiting BG.

--- BGKB/BGKB.lua
+++ BGKB/BGKB.lua
@@ -1,100 +1,119 @@
 -- Globals
-UpdateInterval = .5;
-UpdateNumber = 0;
-KillCount = 0;
-DeathCount = 0;
-InBG = 1;
-KillSounds = {"shinji","ohgod","finnatry","fired","gameover","son"};
-DeathSounds = {"ha","hum","ogre"};
-KillStreakSounds = {"doublekill2","triplekill","ultrakill","stoopid"};
-DeathStreakSounds = {"yo","yo2","yo3"};
+UpdateInterval = 5
+KillCount = 0
+DeathCount = 0
+InBG = 0
+KillSounds = {"shinji","ohgod","finnatry","fired","gameover","son"}
+DeathSounds = {"ha","hum","ogre"}
+KillStreakSounds = {"doublekill2","triplekill","ultrakill","stoopid"}
+DeathStreakSounds = {"yo","yo2","yo3"}
 
 -- Functions
 function HelloWorld()
-	print("KillingBlowDetected Loaded");
-	RandSound(KillStreakSounds);
+	print("KillingBlowDetected Loaded")
+	print("running")
+end
+
+local function BGCheck()
+	local index = 1
+	local numBattlefields = GetMaxBattlefieldID()
+	local status, mapName, instanceID, lowestLevel, highestLevel, teamSize, registeredMatch
+	for i = 1, numBattlefields, 1 do
+ 		status, mapName, instanceID, lowestLevel, highestLevel, teamSize = GetBattlefieldStatus(i)
+		-- print(status, mapName, instanceID, lowestLevel, highestLevel, teamSize, registeredMatch)
+		if status == "active" then
+			if InBG == 0 then
+				print("Entering BG")
+			end
+			InBG = 1
+		else
+			if InBG == 1 then
+				print("Leaving BG")
+			end
+			InBG = 0
+			KillCount = 0
+			DeathCount = 0
+		end
+	end
+	-- InBG = 1
 end
 
 function EventHandler(self, event, ...)
 	if InBG == 1 then
 		if event == "PLAYER_PVP_KILLS_CHANGED" then
-			print("Wrecked");
+			print("Wrecked")
 		end
 		if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-			CheckKillingBlow( ... );
+			CheckKillingBlow( ... )
 		end
 		if event == "PLAYER_DEAD" then
-			KillCount = 0;
-			DeathCount = DeathCount + 1;
-			temp = RandSound(KillSounds);
-			temp = "Interface\\AddOns\\BGKB\\sound\\kills\\" .. KillSounds[temp] .. ".mp3";
-			PlaySoundFile(temp);
+			KillCount = 0
+			DeathCount = DeathCount + 1
+			local temp = RandSound(KillSounds)
+			temp = "Interface\\AddOns\\BGKB\\sound\\kills\\" .. DeathSounds[temp] .. ".mp3"
+			PlaySoundFile(temp)
 		end
 		if event == "PLAYER_PVP_KILLS_CHANGED" then
-			temp = RandSound(KillSounds);
-			temp = "Interface\\AddOns\\BGKB\\sound\\kills\\" .. KillSounds[temp] .. ".mp3";
-			PlaySoundFile(temp);
+			local temp = RandSound(KillSounds)
+			temp = "Interface\\AddOns\\BGKB\\sound\\kills\\" .. KillSounds[temp] .. ".mp3"
+			PlaySoundFile(temp)
 		end
 	end
 end
 
 function CheckKillingBlow( ... )
-	local ag1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10 = ...;
+	local ag1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10 = ...
 	
 	if arg2 == "PARTY_KILL" and arg5 == UnitName("player") then
 		--Used for debugging which arg is which from combat log
-		-- print("Timestamp: ", arg1);
-		-- print("event: ", arg2);
-		-- print("sourceGUID: ", arg4);
-		-- print("sourceName: ", arg5);
-		-- print("sourceFlags: ", arg7);
-		-- print("destGUID: ", arg8);
-		-- print("destName: ", arg9);
-		-- print("destFlags: ", arg10);
-		print(arg5, " has killed ", arg9);
-		KillCount = KillCount + 1;
-		DeathCount = 0;
+		-- print("Timestamp: ", arg1)
+		-- print("event: ", arg2)
+		-- print("sourceGUID: ", arg4)
+		-- print("sourceName: ", arg5)
+		-- print("sourceFlags: ", arg7)
+		-- print("destGUID: ", arg8)
+		-- print("destName: ", arg9)
+		-- print("destFlags: ", arg10)
+		print(arg5, " has killed ", arg9)
+		KillCount = KillCount + 1
+		DeathCount = 0
 		if KillCount == 1 then
-			PlaySoundFile("Interface\\AddOns\\BGKB\\sound\\killstreaks\\firstblood.mp3");
+			PlaySoundFile("Interface\\AddOns\\BGKB\\sound\\killstreaks\\firstblood.mp3")
 		elseif KillCount == 2 then
-			PlaySoundFile("Interface\\AddOns\\BGKB\\sound\\killstreaks\\doublekill2.mp3");
+			PlaySoundFile("Interface\\AddOns\\BGKB\\sound\\killstreaks\\doublekill2.mp3")
 		elseif KillCount == 3 then
-			PlaySoundFile("Interface\\AddOns\\BGKB\\sound\\killstreaks\\triplekill.mp3");
+			PlaySoundFile("Interface\\AddOns\\BGKB\\sound\\killstreaks\\triplekill.mp3")
 		elseif KillCount == 4 then
-			PlaySoundFile("Interface\\AddOns\\BGKB\\sound\\killstreaks\\ultrakill.mp3");
+			PlaySoundFile("Interface\\AddOns\\BGKB\\sound\\killstreaks\\ultrakill.mp3")
 		else
-			temp = RandSound(KillSounds);
-			temp = "Interface\\AddOns\\BGKB\\sound\\kills\\" .. KillSounds[temp] .. ".mp3";
-			PlaySoundFile(temp);
+			temp = RandSound(KillSounds)
+			temp = "Interface\\AddOns\\BGKB\\sound\\kills\\" .. KillSounds[temp] .. ".mp3"
+			PlaySoundFile(temp)
 		end
 		
-		print("Kill Streak: ", KillCount);	
+		print("Kill Streak: ", KillCount)	
 	end
 end
 
 function RandSound(list)
-	len = table.getn(list);
-	print(len);
-	val = math.random(0,len);
-	print(val);
-	return val;
+	local len = table.getn(list)
+	local val = math.random(0,len)
+	return val
+end
+
+function BGKB_OnUpdate(self, elapsed)
+  self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed
+  if self.TimeSinceLastUpdate > UpdateInterval then
+  	BGCheck()
+    self.TimeSinceLastUpdate = 0
+  end
 end
 
 -- function GetSoundFiles()
--- 	current_dir = io.popen"cd":read'*l';
--- 	print(current_dir);
+-- 	current_dir = io.popen"cd":read'*l'
+-- 	print(current_dir)
 -- 	for file in io.popen([[dir ":\Program Files\"World of Warcraft"\Interface\AddOns\BGKB\sound\death" /b /ad]]) do 
--- 		print(file);
+-- 		print(file)
 -- 	end
 -- end
 
--- local function BGKB_OnUpdate(self, elapsed)
---   self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed; 	
-
---   if (self.TimeSinceLastUpdate > UpdateInterval) then
---   	UpdateNumber = UpdateNumber + 1;
---     print("Updating ", UpdateNumber);
-
---     self.TimeSinceLastUpdate = 0;
---   end
--- end
